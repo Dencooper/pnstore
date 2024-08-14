963550f450bda993693f4beb6d308971b320e18f
namle <lephuongnam958506@gmail.com>
2024-08-14 15:50:52 +0700

    initalize the project

---
+++ src/main/java/com/dencooper/pnstore/PnstoreApplication.java
@@ -0,0 +1,11 @@
+package com.dencooper.pnstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnstoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PnstoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dencooper/pnstore/PnstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dencooper.pnstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
